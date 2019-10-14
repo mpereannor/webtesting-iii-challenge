030e6ef102fe1660acac8d9de9e3b5845bafa0e9
chudnofskyATD <mpereannor@gmail.com>
2019-10-14 22:14:06 +0000

    dashboard tested

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,27 @@
 // Test away!
+import React from 'react';
+
+import * as rtl from '@testing-library/react';
+
+import '@testing-library/jest-dom/extend-expect';
+
+import Controls from './Controls';
+
+afterEach(rtl.cleanup);
+
+let wrapper;
+
+beforeEach(() => {
+    wrapper = rtl.render(<Controls/>);
+})
+
+describe('Controls Component', () => {
+    it ('renders without crashing', () => {
+
+    })
+
+    it ('defaults to `unlocked` ', () => {
+
+
+    })
+})
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,20 @@
 // Test away!
+import * as rtl from '@testing-library/react';
+
+import '@testing-library/jest-dom/extend-expect';
+
+import Display from './Display';
+
+afterEach(rtl.cleanup);
+
+let wrapper;
+
+beforeEach(() => {
+    wrapper = rtl.render(Display);
+})
+
+describe('Display Component', () => {
+    it ('renders without crashing', () => {
+
+    })
+})
